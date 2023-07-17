153cc08bed446ec113355942e4d87199f717802b
jhkim1102 <jhkim1102@naver.com>
2023-07-17 12:35:18 +0900

    up

---
+++ src/test/java/com/example/gitexam/GitexamApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.gitexam;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GitexamApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
